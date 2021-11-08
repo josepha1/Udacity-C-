a9e5b860acfc17c955edaf11e0a34a8213d71998
Joseph Allen <josepha1@email.arizona.edu>
2021-11-07 20:59:26 -0700

    First Commit

---
+++ Introduction to the C++ Language/Grid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using std::vector;
+using std::cout;
+
+int main()
+{
+    // Declare a board variable to store the necessary data in a vector.
+    vector<vector<int>> board = {{0, 1, 0, 0, 0, 0},
+                                 {0, 1, 0, 0, 0, 0},
+                                 {0, 1, 0, 0, 0, 0},
+                                 {0, 1, 0, 0, 0, 0},
+                                 {0, 0, 0, 0, 1, 0}};
+        
+}
---
+++ Introduction to the C++ Language/Vectors.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using std::vector;
+using std::cout;
+
+int main()
+{
+    // Three ways of declaring and initializing vectors.
+    vector<int> v_1 {0, 1, 2};
+    vector<int> v_2 = {3, 4, 5};
+    vector<int> v_3;
+    v_3 = {6};
+    cout << "Everything worked!" << "\n";
+
+    // Creating a 2D vector.
+    vector<vector<int>> v {{1, 2}, {3, 4}};
+    cout << "Great! A 2D vector has been created." << "\n";
+}
