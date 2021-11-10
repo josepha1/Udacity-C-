ebe781e863b216a81f255204cbef4e95d58ecd19
Joseph <josepha1@email.arizona.edu>
2021-11-10 13:40:01 -0700

    Second Commit

--- Introduction to the C++ Language/Vectors.cpp
+++ Introduction to the C++ Language/Vectors.cpp
@@ -15,4 +15,23 @@ int main()
     // Creating a 2D vector.
     vector<vector<int>> v {{1, 2}, {3, 4}};
     cout << "Great! A 2D vector has been created." << "\n";
+
+    // 1D Vector Access
+    vector<int> a = {0, 1, 2, 3, 4};
+    cout << a[0];
+    cout << a[1];
+    cout << a[2];
+    cout << "\n";
+
+    // 2D Vector Access
+    vector<vector<int>> b = {{1, 1, 2, 3},
+                             {2, 1, 2, 3},
+                             {3, 1, 2, 3}};
+
+    // Get vector size (i.e. length of the vector)
+    cout << a.size();
+
+    // Get 2D vector size
+    cout << b[0].size() << "\n";
+
 }
