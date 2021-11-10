eecb7e7a6e7fdc5120e85580fa6a8212a15f99d4
Joseph <josepha1@email.arizona.edu>
2021-11-10 14:56:21 -0700

    Added PrintBoard Function

--- Introduction to the C++ Language/Grid.cpp
+++ Introduction to the C++ Language/Grid.cpp
@@ -3,6 +3,19 @@
 using std::vector;
 using std::cout;
 
+void PrintBoard(vector<vector<int>> board)
+{
+    for (auto v : board)
+    {
+        for (int i : v)
+        {
+            cout << i;
+        }
+
+        cout << "\n";
+    }
+}
+
 int main()
 {
     // Declare a board variable to store the necessary data in a vector.
@@ -11,5 +24,7 @@ int main()
                                  {0, 1, 0, 0, 0, 0},
                                  {0, 1, 0, 0, 0, 0},
                                  {0, 0, 0, 0, 1, 0}};
+
+    PrintBoard(board);
         
 }
