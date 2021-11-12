458c1402fda008f08d2684e4082d324857f72236
Joseph <josepha1@email.arizona.edu>
2021-11-11 20:10:46 -0700

    Second Commit

--- Introduction to the C++ Language/File_Reading.cpp
+++ Introduction to the C++ Language/File_Reading.cpp
@@ -2,6 +2,14 @@
 #include <iostream>
 #include <string>
 
+/*
+STEPS TO READING A FILE:
+1. #include <fstream>
+2. Create a std::ifstream object using the path of your file.
+3. Evaluate the std::ifstream object as a bool to ensure that the stream creation did not fail.
+4. Use a while loop with getline to write file line to a string.
+*/
+
 int main()
 {
     std::ifstream my_file;
@@ -9,5 +17,10 @@ int main()
     if (my_file)
     {
         std::cout << "The file stream has been created!" << "\n";
+        std::string line;
+        while (getline(my_file, line))
+        {
+            std::cout << line << "\n";
+        }
     }
 }
